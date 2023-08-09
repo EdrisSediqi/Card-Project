6e4501fcbcf0224e08dcc2aeae826ec33a2b9d61
EdrisSediqi <123951228+EdrisSediqi@users.noreply.github.com>
2023-08-09 10:46:26 +0430

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans:['Hanken Grotesk', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
